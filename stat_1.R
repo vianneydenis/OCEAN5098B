6a10813fe75b882b1621c2c1a0ceef8b4896f8bf
vianneydenis <vianney.denis@gmail.com>
2024-11-08 14:38:27 +0800

    update stat1

--- stat_1.R
+++ stat_1.R
@@ -3,10 +3,11 @@ xaringanExtra::use_clipboard()
 
 
 library (psych)
-library(ggplot2)
-library(dplyr)
+library (tidyverse)
 library(gridExtra)
 library(car)
+library(ggpubr)
+library(rstatix)
 
 softies<-read.csv('data/sf_res.csv', sep=',', header=T)
 str(softies)
@@ -85,78 +86,95 @@ s1<-students[,1:2] # a matrix
 # cor(s1)
 cor.test(x1,y1)
 
-## # HO: no significant relationship between the amounts of chl a and c
-## # H1: significant relationship between the amounts of chl a and c
-## x2<-softies$chla.afdw
-## y2<-softies$chlc.afdw
-## s2<-softies[,c(12,14)] # a matrix
-## # Pearson correlation
-## # cor(x,y)
-## # cor(s)
-## cor.test(x2,y2)
+# HO: no significant relationship between the amounts of chl a and c
+# H1: significant relationship between the amounts of chl a and c
+x2<-softies$chla.afdw
+y2<-softies$chlc.afdw
+s2<-softies[,c(12,14)] # a matrix
+# Pearson correlation
+# cor(x,y)
+# cor(s)
+cor.test(x2,y2)
 
-## ggplot(softies, aes(x = chla.afdw, y = chlc.afdw)) +
-##   geom_point() +
-##   stat_smooth(method = "lm", col = "red")
+ggplot(softies, aes(x = chla.afdw, y = chlc.afdw)) + 
+  geom_point() +
+  stat_smooth(method = "lm", col = "red")
 
-## w<-(1:100)
-## z<-exp(x)
-## cor.test(w,z,method='spearman')
-## # cor.test(w,z,method='pearson')
+w<-(1:100)
+z<-exp(w)
+cor.test(w,z,method='spearman')
+# cor.test(w,z,method='pearson') 
 
-## #Cast 240 times a die. We counted occurrence of 1,2,3,4,5,6
-## die<-data.frame(obs=c(55,44,35,45,31,30), row.names=c('Cast1','Cast2','Cast3','Cast4','Cast5','Cast6'))
-## chisq.test(die)
+#Cast 240 times a die. We counted occurrence of 1,2,3,4,5,6
+die<-data.frame(obs=c(55,44,35,45,31,30), row.names=c('Cast1','Cast2','Cast3','Cast4','Cast5','Cast6'))
+chisq.test(die)
 
-## obs <- c(750, 50, 200)
-## exp <- c(0.60, 0.35, 0.05)
-## chisq.test (x=obs, p=exp)
+obs <- c(750, 50, 200)
+exp <- c(0.60, 0.35, 0.05)
+chisq.test (x=obs, p=exp)
 
-## F <- matrix(nrow=4,ncol=2,data=c(33,14, 8,18,31,25,14,12))
-## chisq.test(F) # alternative see `fisher.test`
+F <- matrix(nrow=4,ncol=2,data=c(33,14, 8,18,31,25,14,12))
+chisq.test(F) # alternative see `fisher.test`
 
-## # One sample
-## t.test (softies$ratio, mu=0.4)
-## # Two sample (with equal variances)
-## t.test (softies$ratio~softies$species, var.equal = TRUE)
-## # Two sample (with unequal variances)
-## t.test (softies$chla.afdw~softies$species)
-## # Two sample paired t.test
-## # ?sleep
-## sleep2 <- reshape(sleep, direction = "wide", idvar = "ID", timevar = "group")
-## t.test (sleep2$extra.1,sleep2$extra.2,paired=T )
-## # same as: t.test(Pair(extra.1, extra.2) ~ 1, data = sleep2)
+# One sample
+t.test (softies$ratio, mu=0.4)
+# Two sample (with equal variances)
+t.test (softies$ratio~softies$species, var.equal = TRUE)
+# Two sample (with unequal variances) 
+t.test (softies$chla.afdw~softies$species)
+# Two sample paired t.test
+# ?sleep
+sleep2 <- reshape(sleep, direction = "wide", idvar = "ID", timevar = "group")
+t.test (sleep2$extra.1,sleep2$extra.2,paired=T )
+# same as: t.test(Pair(extra.1, extra.2) ~ 1, data = sleep2)
 
 ## set <- iris[iris$Species == "setosa", ]$Sepal.Length
 ## ver <- iris[iris$Species == "versicolor", ]$Sepal.Length
 ## boxplot(set, ver)
 ## setver <- t.test(set, ver, var.equal = TRUE, paired = FALSE)
 
-## 
-## ## One-sample test.
-## ## Hollander & Wolfe (1973), 29f.
-## ## Hamilton depression scale factor measurements in 9 patients with
-## ##  mixed anxiety and depression, taken at the first (x) and second
-## ##  (y) visit after initiation of a therapy (administration of a
-## ##  tranquilizer).
-## x <- c(1.83,  0.50,  1.62,  2.48, 1.68, 1.88, 1.55, 3.06, 1.30)
-## y <- c(0.878, 0.647, 0.598, 2.05, 1.06, 1.29, 1.06, 3.14, 1.29)
-## depression <- data.frame(first = x, second = y, change = y - x)
-## wilcox.test(change ~ 1, data = depression) # Formula interface to one-sample
-## wilcox.test(Pair(first, second) ~ 1, data = depression) # Formula interface to paired tests
-## 
-## ## Two-sample test.
-## ### Ozone
-## boxplot(Ozone ~ Month, data = airquality)
-## wilcox.test(Ozone ~ Month, data = airquality, subset = Month %in% c(5, 8))
-## ### wilcox_test in package coin for exact, asymptotic and Monte Carlo conditional p-values, including in the presence of ties.
-## 
-## ## Softies
-## boxplot(prot.afdw~species, data=softies)
-## shapiro.test(softies$prot.afdw) # data are not normal
-## qqnorm(softies$prot.afdw)
-## qqline(softies$prot.afdw)
-## wilcox.test (softies$prot.afdw~softies$species)
+## One-sample test.
+## Hollander & Wolfe (1973), 29f.
+## Hamilton depression scale factor measurements in 9 patients with
+##  mixed anxiety and depression, taken at the first (x) and second
+##  (y) visit after initiation of a therapy (administration of a
+##  tranquilizer).
+x <- c(1.83,  0.50,  1.62,  2.48, 1.68, 1.88, 1.55, 3.06, 1.30)
+y <- c(0.878, 0.647, 0.598, 2.05, 1.06, 1.29, 1.06, 3.14, 1.29)
+depression <- data.frame(first = x, second = y, change = y - x)
+wilcox.test(change ~ 1, data = depression) # Formula interface to one-sample
+wilcox.test(Pair(first, second) ~ 1, data = depression) # Formula interface to paired tests
+
+## Two-sample test.
+### Ozone
+boxplot(Ozone ~ Month, data = airquality)
+wilcox.test(Ozone ~ Month, data = airquality, subset = Month %in% c(5, 8))
+### wilcox_test in package coin for exact, asymptotic and Monte Carlo conditional p-values, including in the presence of ties. 
+
+## Softies
+boxplot(prot.afdw~species, data=softies)
+shapiro.test(softies$prot.afdw) # data are not normal
+qqnorm(softies$prot.afdw) 
+qqline(softies$prot.afdw)
+wilcox.test (softies$prot.afdw~softies$species) 
+
+kruskal.test(Ozone ~ Month, data = airquality)
+# see also from rstatix
+res.kruskal <- airquality %>% kruskal_test(Ozone ~ Month)
+res.kruskal
+
+# Pairwise comparisons
+oz <- airquality %>% 
+  dunn_test(Ozone ~ Month, p.adjust.method = "bonferroni") 
+oz
+
+oz <- oz %>% add_xy_position(x = "Month")
+ggboxplot(airquality, x = "Month", y = "Ozone") +
+  stat_pvalue_manual(oz, hide.ns = TRUE) +
+  labs(
+    subtitle = get_test_label(res.kruskal, detailed = TRUE),
+    caption = get_pwc_label(oz)
+    )
 
 ## # Test of variance: we test HO: homogeneous, H1:heterogeneous
 ## fligner.test (softies$ratio ~ softies$species)
