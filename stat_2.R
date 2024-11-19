839179b8da364c27b2472ccf3f194a414f27cc98
vianneydenis <vianney.denis@gmail.com>
2024-11-19 09:38:16 +0800

    typos

--- stat_2.R
+++ stat_2.R
@@ -34,11 +34,11 @@ confint(mod1_plant)
 
 sigma(mod1_plant)*100/mean(Plant_height$loght)
 
-## fitted(fit1) # predicted values
-## residuals(fit1) # residuals
-## anova(fit1) # anova table
-## vcov(fit1) # covariance matrix for model parameters
-## influence(fit1) # regression diagnostics
+## fitted(mod1_plant) # predicted values
+## residuals(mod1_plant) # residuals
+## anova(mod1_plant) # anova table
+## vcov(mod1_plant) # covariance matrix for model parameters
+## influence(mod1_plant) # regression diagnostics
 
 plot(mod1_plant, which = 1)
 
