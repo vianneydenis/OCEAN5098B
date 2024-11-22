c0e9dcb2768e75512a53753d85e93d6d1e9b5c0d
vianneydenis <vianney.denis@gmail.com>
2024-11-22 17:49:28 +0800

    LMM

--- stat_2.R
+++ stat_2.R
@@ -14,11 +14,14 @@ xaringanExtra::use_clipboard()
 
 
 
+
+
 library(gvlma)
 library(Hmisc)
 library(corrplot)
 library(mvabund)
 library(ggplot2)
+library (lme4)
 
 knitr::include_graphics("illustrations/linear-regression.png")
 
@@ -150,3 +153,183 @@ summary(ft.sol.nb)
 ft.sol.nb$coefficients
 
 boxplot(Soleolifera ~ Treatment, ylab = "Count", xlab = "Treatment", data = Reveg)
+
+Estuaries <- read.csv("data/estuaries.csv", header = T)
+
+ft.estu <- lmer(Total ~ Modification + (1 | Estuary), data = Estuaries, REML = T)
+
+qqnorm(residuals(ft.estu))
+
+scatter.smooth(residuals(ft.estu) ~ fitted(ft.estu))
+
+ft.estu <- lmer(Total ~ Modification + (1 | Estuary), data = Estuaries, REML = F)
+ft.estu.0 <- lmer(Total ~ (1 | Estuary), data = Estuaries, REML = F)
+
+anova(ft.estu.0, ft.estu)
+
+confint(ft.estu)
+
+nBoot <- 1000
+lrStat <- rep(NA, nBoot)
+ft.null <- lm(Total ~ Modification, data = Estuaries) # null model
+ft.alt <- lmer(Total ~ Modification + (1 | Estuary), data = Estuaries, REML = F) # alternate model
+lrObs <- 2 * logLik(ft.alt) - 2 * logLik(ft.null) # observed test stat
+for (iBoot in 1:nBoot)
+{
+  Estuaries$TotalSim <- unlist(simulate(ft.null)) # resampled data
+  bNull <- lm(TotalSim ~ Modification, data = Estuaries) # null model
+  bAlt <- lmer(TotalSim ~ Modification + (1 | Estuary), data = Estuaries, REML = F) # alternate model
+  lrStat[iBoot] <- 2 * logLik(bAlt) - 2 * logLik(bNull) # resampled test stat
+}
+mean(lrStat > lrObs) # P-value for test of Estuary effect
+
+ModEst <- unique(Estuaries[c("Estuary", "Modification")]) # find which Estuaries are modified
+
+# Prepare a vector of colors with specific color by modification levels
+myColors <- ifelse(unique(ModEst$Modification) == "Modified", rgb(0.1, 0.1, 0.7, 0.5),
+  ifelse(unique(ModEst$Modification) == "Pristine", rgb(0.8, 0.1, 0.3, 0.6),
+    "grey90"
+  )
+)
+
+boxplot(Total ~ Estuary, data = Estuaries, col = myColors, xlab = "Estuary", ylab = "Total invertebrates")
+legend("bottomleft",
+  inset = .02,
+  c(" Modified ", " Pristine "), fill = unique(myColors), horiz = TRUE, cex = 0.8
+)
+
+# 0 if Modified, 1 if Pristine
+is.mod <- ifelse(unique(ModEst$Modification) == "Modified", 0,
+  ifelse(unique(ModEst$Modification) == "Pristine", 1, NA)
+)
+
+Est.means <- coef(ft.estu)$Estuary[, 1] + coef(ft.estu)$Estuary[, 2] * is.mod # Model means
+
+stripchart(Est.means ~ sort(unique(Estuary)), data = Estuaries, pch = 18, col = "red", vertical = TRUE, add = TRUE)
+
+Estuaries[1:10, ]
+xtabs(~ Estuary + Site, Estuaries, sparse = TRUE)
+
+Estuaries$Site <- as.factor(Estuaries$Site)
+Estuaries$SiteWithin <- paste0(Estuaries$Estuary, "_", Estuaries$Site)
+
+xtabs(~ Estuary + SiteWithin, Estuaries, sparse = TRUE)
+
+fit.mod <- lmer(Total ~ Modification + (1 | Estuary) + (1 | SiteWithin), data = Estuaries)
+# same as: lmer(Total ~ Modification + (1 | Estuary/SiteWithin), data = Estuaries)
+summary(fit.mod)
+
+fit.wrong <- lmer(Total ~ Modification + (1 | Estuary) + (1 | Site), data = Estuaries)
+summary(fit.wrong)
+
+par(mfrow = c(1, 2))
+qqnorm(residuals(fit.mod))
+scatter.smooth(residuals(fit.mod) ~ fitted(fit.mod)) # residual plot
+
+fit.mod <- lmer(log(Total) ~ Modification + (1 | Estuary) + (1 | SiteWithin), data = Estuaries)
+par(mfrow = c(1, 2))
+qqnorm(residuals(fit.mod))
+scatter.smooth(residuals(fit.mod) ~ fitted(fit.mod)) # residual plot
+
+ft.mod <- lmer(log(Total) ~ Modification + (1 | Estuary) + (1 | SiteWithin), data = Estuaries, REML = F)
+ft.mod.0 <- lmer(log(Total) ~ (1 | Estuary) + (1 | SiteWithin), data = Estuaries, REML = F)
+anova(ft.mod.0, ft.mod)
+
+nBoot <- 1000
+lrStat <- rep(NA, nBoot)
+ft.null <- lmer(log(Total) ~ Modification + (1 | Estuary), Estuaries, REML = F) # null model
+ft.alt <- lmer(log(Total) ~ Modification + (1 | Estuary) + (1 | SiteWithin), Estuaries, REML = F) # alternate model
+lrObs <- 2 * logLik(ft.alt) - 2 * logLik(ft.null) # observed test stat
+for (iBoot in 1:nBoot)
+{
+  Estuaries$logTotalSim <- unlist(simulate(ft.null)) # resampled data
+  bNull <- lmer(logTotalSim ~ Modification + (1 | Estuary), Estuaries, REML = F) # null model
+  bAlt <- lmer(logTotalSim ~ Modification + (1 | Estuary) + (1 | SiteWithin), Estuaries, REML = F) # alternate model
+  lrStat[iBoot] <- 2 * logLik(bAlt) - 2 * logLik(bNull) # resampled test stat
+}
+mean(lrStat > lrObs) # P-value for test of Estuary effect
+
+Estuaries$Hydroid
+
+Estuaries$HydroidPres <- Estuaries$Hydroid > 0
+
+fit.bin <- glmer(HydroidPres ~ Modification + (1 | Estuary), family = binomial, data = Estuaries)
+
+par(mfrow = c(1, 2))
+plot(residuals(fit.bin) ~ fitted(fit.bin), main = "residuals v.s. Fitted")
+qqnorm(residuals(fit.bin))
+
+nBoot <- 1000
+lrStat <- rep(NA, nBoot)
+ft.null <- glmer(HydroidPres ~ 1 + (1 | Estuary), family = binomial, data = Estuaries) # null model
+ft.alt <- glmer(HydroidPres ~ Modification + (1 | Estuary), family = binomial, data = Estuaries) # alternate model
+
+lrObs <- 2 * logLik(ft.alt) - 2 * logLik(ft.null) # observed test stat
+
+for (iBoot in 1:nBoot)
+{
+  Estuaries$HydroidPresSim <- unlist(simulate(ft.null)) # resampled data
+  tryCatch(
+    { # sometimes the glmer code doesn't converge
+
+      bNull <- glmer(HydroidPresSim ~ 1 + (1 | Estuary), family = binomial, data = Estuaries) # null model
+      bAlt <- glmer(HydroidPresSim ~ Modification + (1 | Estuary), family = binomial, data = Estuaries) # alternate model
+      lrStat[iBoot] <- 2 * logLik(bAlt) - 2 * logLik(bNull) # resampled test stat
+    },
+    warning = function(war) {
+      lrStat[iBoot] <- NA
+    },
+    error = function(err) {
+      lrStat[iBoot] <- NA
+    }
+  ) # if code doesn't converge skip sim
+}
+mean(lrStat > lrObs, na.rm = T) # P-value for test of Estuary effect
+
+fit.pois <- glmer(Hydroid ~ Modification + (1 | Estuary), family = poisson, data = Estuaries)
+
+par(mfrow = c(1, 2))
+plot(residuals(fit.pois) ~ fitted(fit.pois), main = "Residuals vs. Fitted")
+qqnorm(residuals(fit.pois))
+
+nBoot <- 1000
+lrStat <- rep(NA, nBoot)
+ft.null <- glmer(Hydroid ~ 1 + (1 | Estuary), family = poisson, data = Estuaries) # null model
+ft.alt <- glmer(Hydroid ~ Modification + (1 | Estuary), family = poisson, data = Estuaries) # alternate model
+
+lrObs <- 2 * logLik(ft.alt) - 2 * logLik(ft.null) # observed test stat
+for (iBoot in 1:nBoot)
+{
+  Estuaries$HydroidSim <- unlist(simulate(ft.null)) # resampled data
+  tryCatch(
+    {
+      bNull <- glmer(HydroidSim ~ 1 + (1 | Estuary), family = poisson, data = Estuaries) # null model
+      bAlt <- glmer(HydroidSim ~ Modification + (1 | Estuary), family = poisson, data = Estuaries) # alternate model
+      lrStat[iBoot] <- 2 * logLik(bAlt) - 2 * logLik(bNull) # resampled test stat
+    },
+    warning = function(war) {
+      lrStat[iBoot] <- NA
+    },
+    error = function(err) {
+      lrStat[iBoot] <- NA
+    }
+  ) # if code doesn't converge skip sim#   lrStat[iBoot]
+}
+
+fit.pois2 <- glmer(Schizoporella.errata ~ Modification + (1 | Estuary), family = poisson, data = Estuaries)
+par(mfrow = c(1, 2))
+plot(residuals(fit.pois) ~ fitted(fit.pois), main = "residuals vs. Fitted")
+qqnorm(residuals(fit.pois))
+
+fit.pois <- glmer(Hydroid ~ Modification + (1 | Estuary), family = poisson, data = Estuaries)
+means <- fitted(fit.pois) # this will give the estimate at each data point
+ModEst <- unique(Estuaries[c("Estuary", "Modification")]) # find which Estuaries are modified
+cols <- as.numeric(ModEst[order(ModEst[, 1]), 2]) + 3 # Assign colour by modification
+boxplot(Hydroid ~ Estuary, data = Estuaries, col = cols, xlab = "Estuary", ylab = "Count of hydroids")
+legend("topleft",
+  inset = .02,
+  c("Modified", "Pristine"), fill = unique(cols), horiz = TRUE, cex = 0.8
+)
+
+Est.means <- summarize(means, Estuaries$Estuary, mean)$means # extract means by Estuary
+stripchart(Est.means ~ sort(unique(Estuary)), data = Estuaries, pch = 18, col = "red", vertical = TRUE, add = TRUE) # plot means by estuary
